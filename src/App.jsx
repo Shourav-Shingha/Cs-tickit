bc82582f8126d6a55468f2069991a7a318cfb637
Shourav-Shingha <shouravshingha2369@gmail.com>
2026-03-04 13:38:25 +0600

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
 import { ToastContainer } from 'react-toastify';
-
 import './App.css'
+import Navber from './navber/Navber';
 
 function App() {
 
   return (
-    <>
+    <div >
+    <Navber></Navber>
+    <div className='w-11/12 mx-auto bg'>
+
+    </div>
       <ToastContainer />
-    </>
+    </div>
   )
 }
 
